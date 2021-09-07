0330e0b7ec3bce0a6db12c1834dc0d54474db5a0
Edwin Muriithi <edwin.muriithi@student.moringaschool.com>
2021-09-07 13:56:28 +0300

    Update Javascript

---
+++ js/script.js
@@ -0,0 +1,4 @@
+var CC, YY, MM, DD, d, dayValue;
+var dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", " Yaa", "Afua", "Ama"];
